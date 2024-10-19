9e0056889124e3a0e8a8c5f061ead6cc74a044cc
sokuna16 <marinayrandell1@gmail.com>
2024-10-19 11:58:15 +0800

    Update Success

--- IPserverPOS.bat
+++ IPserverPOS.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 :: Check if the virtual environment is already activated
 if not defined VIRTUAL_ENV (
@@ -17,23 +18,34 @@ for /f "tokens=2 delims=:" %%I in ('ipconfig ^| findstr "IPv4"') do (
 set "IP=%IP:~1%"
 
 :: Update settings.py to include the device's IP address in ALLOWED_HOSTS
-setlocal enabledelayedexpansion
 set "file=settings.py"
 set "temp_file=temp_settings.py"
 
 :: Create a temporary file with the updated ALLOWED_HOSTS
+set "ip_found=false"
+
 (
     for /f "usebackq delims=" %%a in ("!file!") do (
         echo %%a
         echo %%a | findstr "ALLOWED_HOSTS" >nul && (
+            set "ip_found=true"
             echo ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '!IP!', '0.0.0.0']
         )
     )
 ) > "!temp_file!"
 
+:: Check if the IP is already in ALLOWED_HOSTS
+if "!ip_found!"=="true" (
+    findstr /c:"!IP!" "!file!" >nul || (
+        echo !IP! added to ALLOWED_HOSTS.
+        echo ALLOWED_HOSTS += ['!IP!'] >> "!temp_file!"
+    )
+)
+
 :: Replace the old settings.py with the new one
 move /Y "!temp_file!" "!file!" >nul
 
+:: Starting the Django development server
 echo Starting the Django development server...
 start python manage.py runserver 0.0.0.0:8000
 
